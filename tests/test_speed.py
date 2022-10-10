08bdd08cd81d08ada038c482fe0b1faf61c6498a
Jake Wall <walljcg@gmail.com>
2022-10-10 21:17:52 +0300

    Update test_speed.py
    removed print statements and using absolute paths

--- tests/test_speed.py
+++ tests/test_speed.py
@@ -5,13 +5,10 @@ import pandas as pd
 import pandas.testing
 import pytest
 import ecoscope
-from ecoscope.analysis import speed
 
 
 def test_speed_geoseries(movbank_relocations):
     trajectory = ecoscope.base.Trajectory.from_relocations(movbank_relocations)
-    sdf = speed.SpeedDataFrame.from_trajectory(trajectory)
-    print(sdf.geometry.is_empty)
-    print(sdf.geometry.isna())
+    sdf = ecoscope.analysis.speed.SpeedDataFrame.from_trajectory(trajectory)
     assert all(~sdf.geometry.is_empty)
     assert all(~sdf.geometry.isna())
