456d91f5a1de03d401e8323c1b406cf17335a290
Eric Gitonga <gitonga@gmail.com>
2023-01-24 05:27:54 +0300

    fix chromedriver colab bug

--- ecoscope/__init__.py
+++ ecoscope/__init__.py
@@ -79,8 +79,81 @@ def init(silent=False, selenium=False, force=False):
     if "google.colab" in sys.modules and selenium:
         import subprocess
 
+        subprocess.run(
+            [
+                "cat",
+                ">",
+                "/etc/apt/sources.list.d/debian.list",
+                "<<'EOF'",
+                """
+                   deb [arch=amd64 signed-by=/usr/share/keyrings/debian-buster.gpg] http://deb.debian.org/debian buster main 
+                   deb [arch=amd64 signed-by=/usr/share/keyrings/debian-buster-updates.gpg] http://deb.debian.org/debian buster-updates main
+                   deb [arch=amd64 signed-by=/usr/share/keyrings/debian-security-buster.gpg] http://deb.debian.org/debian-security buster/updates main
+                   EOF
+                   """,
+            ]
+        )
+
+        subprocess.run(["apt-key", "adv", "--keyserver", "keyserver.ubuntu.com", "--recv-keys", "DCC9EFBF77E11517"])
+        subprocess.run(["apt-key", "adv", "--keyserver", "keyserver.ubuntu.com", "--recv-keys", "648ACFD622F3D138"])
+        subprocess.run(["apt-key", "adv", "--keyserver", "keyserver.ubuntu.com", "--recv-keys", "112695A0E562B32A"])
+
+        subprocess.run(
+            ["apt-key", "export", "77E11517", "|", "gpg", "--dearmour", "-o", "/usr/share/keyrings/debian-buster.gpg"]
+        )
+        subprocess.run(
+            [
+                "apt-key",
+                "export",
+                "77E11517",
+                "|",
+                "gpg",
+                "--dearmour",
+                "-o",
+                "/usr/share/keyrings/debian-buster-updates.gpg",
+            ]
+        )
+        subprocess.run(
+            [
+                "apt-key",
+                "export",
+                "77E11517",
+                "|",
+                "gpg",
+                "--dearmour",
+                "-o",
+                "/usr/share/keyrings/debian-security-buster.gpg",
+            ]
+        )
+
+        subprocess.run(
+            [
+                "cat",
+                ">",
+                "/etc/apt/preferences.d/chromium.pref",
+                "<<'EOF'",
+                """
+                        Package: *
+                        Pin: release a=eoan
+                        Pin-Priority: 500
+                        
+                        
+                        Package: *
+                        Pin: origin 'deb.debian.org'
+                        Pin-Priority: 300
+                        
+                        
+                        Package: chromium*
+                        Pin: origin 'deb.debian.org'
+                        Pin-Priority: 700
+                        EOF
+                        """,
+            ]
+        )
+
         subprocess.run(["apt", "update"])
-        subprocess.run(["apt", "install", "chromium-chromedriver"])
+        subprocess.run(["apt", "install", "chromium", "chromium-driver"])
+        subprocess.run(["pip", "install", "selenium"])
 
     __initialized = True
     if not silent:
