0ce8794a743cbac9624f40157a413b62b7130df7
Eric Gitonga <gitonga@gmail.com>
2023-01-24 14:18:11 +0300

    use shlex to split commands

--- ecoscope/__init__.py
+++ ecoscope/__init__.py
@@ -77,83 +77,69 @@ def init(silent=False, selenium=False, force=False):
     import sys
 
     if "google.colab" in sys.modules and selenium:
+        import shlex
         import subprocess
-
+      
+        cat_1 = shlex.split("cat > /etc/apt/sources.list.d/debian.list <<'EOF'")
         subprocess.run(
             [
-                "cat",
-                ">",
-                "/etc/apt/sources.list.d/debian.list",
-                "<<'EOF'",
+                cat_1,
                 """
-                   deb [arch=amd64 signed-by=/usr/share/keyrings/debian-buster.gpg] http://deb.debian.org/debian buster main 
-                   deb [arch=amd64 signed-by=/usr/share/keyrings/debian-buster-updates.gpg] http://deb.debian.org/debian buster-updates main
-                   deb [arch=amd64 signed-by=/usr/share/keyrings/debian-security-buster.gpg] http://deb.debian.org/debian-security buster/updates main
-                   EOF
-                   """,
-            ]
-        )
+                 deb [arch=amd64 signed-by=/usr/share/keyrings/debian-buster.gpg] http://deb.debian.org/debian buster main
+                 deb [arch=amd64 signed-by=/usr/share/keyrings/debian-buster-updates.gpg] http://deb.debian.org/debian buster-updates main
+                 deb [arch=amd64 signed-by=/usr/share/keyrings/debian-security-buster.gpg] http://deb.debian.org/debian-security buster/updates main
+                 EOF
+                """,
+            ])
 
-        subprocess.run(["apt-key", "adv", "--keyserver", "keyserver.ubuntu.com", "--recv-keys", "DCC9EFBF77E11517"])
-        subprocess.run(["apt-key", "adv", "--keyserver", "keyserver.ubuntu.com", "--recv-keys", "648ACFD622F3D138"])
-        subprocess.run(["apt-key", "adv", "--keyserver", "keyserver.ubuntu.com", "--recv-keys", "112695A0E562B32A"])
+        apt_keyadv_1 = shlex.split("apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DCC9EFBF77E11517")
+        subprocess.run(apt_keyadv_1)
+        
+        apt_keyadv_2 = shlex.split("apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138")
+        subprocess.run(apt_keyadv_2)
+        
+        apt_keyadv_3 = shlex.split("apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 112695A0E562B32A")
+        subprocess.run(apt_keyadv_3)
 
-        subprocess.run(
-            ["apt-key", "export", "77E11517", "|", "gpg", "--dearmour", "-o", "/usr/share/keyrings/debian-buster.gpg"]
-        )
+        apt_keyexport_1 = shlex.split("apt-key export 77E11517 | gpg --dearmour -o /usr/share/keyrings/debian-buster.gpg")
+        subprocess.run(apt_keyexport_1)
+        
+        apt_keyexport_2 = shlex.split("apt-key export 22F3D138 | gpg --dearmour -o /usr/share/keyrings/debian-buster-updates.gpg")
+        subprocess.run(apt_keyexport_2)
+        
+        apt_keyexport_3 = shlex.split("apt-key export E562B32A | gpg --dearmour -o /usr/share/keyrings/debian-security-buster.gpg")
+        subprocess.run(apt_keyexport_3)
+
+        cat_2 = shlex.split("cat > /etc/apt/preferences.d/chromium.pref << 'EOF'")
         subprocess.run(
             [
-                "apt-key",
-                "export",
-                "77E11517",
-                "|",
-                "gpg",
-                "--dearmour",
-                "-o",
-                "/usr/share/keyrings/debian-buster-updates.gpg",
-            ]
-        )
-        subprocess.run(
-            [
-                "apt-key",
-                "export",
-                "77E11517",
-                "|",
-                "gpg",
-                "--dearmour",
-                "-o",
-                "/usr/share/keyrings/debian-security-buster.gpg",
-            ]
-        )
-
-        subprocess.run(
-            [
-                "cat",
-                ">",
-                "/etc/apt/preferences.d/chromium.pref",
-                "<<'EOF'",
+                cat_2,
                 """
-                        Package: *
-                        Pin: release a=eoan
-                        Pin-Priority: 500
-                        
-                        
-                        Package: *
-                        Pin: origin 'deb.debian.org'
-                        Pin-Priority: 300
-                        
-                        
-                        Package: chromium*
-                        Pin: origin 'deb.debian.org'
-                        Pin-Priority: 700
-                        EOF
-                        """,
-            ]
-        )
-
-        subprocess.run(["apt", "update"])
-        subprocess.run(["apt", "install", "chromium", "chromium-driver"])
-        subprocess.run(["pip", "install", "selenium"])
+                  Package: *
+                  Pin: release a=eoan
+                  Pin-Priority: 500
+                  
+                  
+                  Package: *
+                  Pin: origin "deb.debian.org"
+                  Pin-Priority: 300
+                  
+                  
+                  Package: chromium*
+                  Pin: origin "deb.debian.org"
+                  Pin-Priority: 700
+                  EOF
+                """,
+            ])
+  
+        apt_update = shlex.split("apt-get update")
+        subprocess.run(apt_update)
+        
+        apt_install = shlex.split("apt-get install chromium chromium-driver")
+        subprocess.run(apt_install)
+        
+        pip_install = shlex.split("pip install selenium")
+        subprocess.run(pip_install)
 
     __initialized = True
     if not silent:
