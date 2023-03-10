04cb04c210d21f106a79ed05c9be416fa6ff20e2
Eric Gitonga <gitonga@gmail.com>
2023-03-10 15:08:46 +0300

    Update earthranger.py

--- ecoscope/io/earthranger.py
+++ ecoscope/io/earthranger.py
@@ -313,7 +313,7 @@ class EarthRangerIO(ERClient):
         for _id in pbar:
             params[id_name] = _id
             pbar.set_description(f"Downloading Observations for {id_name}={_id}")
-            dataframe = self.get_objects_multithreaded(params=params, object="observations")
+            dataframe = pd.DataFrame(self.get_objects_multithreaded(params=params, object="observations"))
             dataframe[id_name] = _id
             observations.append(dataframe)
 
