d787c1d4a7f87915c24ff080a54f366557247a15
Eric Gitonga <gitonga@gmail.com>
2023-04-07 21:46:29 +0300

    get_subjectsource() call get_objects_multithreaded

--- ecoscope/io/earthranger.py
+++ ecoscope/io/earthranger.py
@@ -184,7 +184,7 @@ class EarthRangerIO(ERClient):
         subjectsources : pd.DataFrame
         """
         params = self._clean_kwargs(addl_kwargs, sources=sources, subjects=subjects)
-        return self.get_objects_multithreaded(object="subjectsources/", **params)
+        return pd.DataFrame(self.get_objects_multithreaded(object="subjectsources/", **params))
 
 
     def _get_observations(
