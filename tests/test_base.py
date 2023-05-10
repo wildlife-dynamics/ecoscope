0e1468ef818ee152fc06e77f9c60df962b364734
Eric <gitonga@gmail.com>
2023-05-11 00:57:14 +0300

    switch from movbank to mep-dev

--- tests/test_base.py
+++ tests/test_base.py
@@ -8,34 +8,39 @@ import pytest
 import ecoscope
 
 
-def test_redundant_columns_in_trajectory(movbank_relocations):
+def test_redundant_columns_in_trajectory(er_io):
     # test there is no redundant column in trajectory
-    trajectory = ecoscope.base.Trajectory.from_relocations(movbank_relocations)
+    relocations = er_io.get_subjectgroup_observations(group_name=er_io.GROUP_NAME)
+    trajectory = ecoscope.base.Trajectory.from_relocations(relocations)
     assert "extra__fixtime" not in trajectory
     assert "extra___fixtime" not in trajectory
     assert "extra___geometry" not in trajectory
 
 
-def test_relocs_speedfilter(movbank_relocations):
+def test_relocs_speedfilter(er_io):
+    relocations = er_io.get_subjectgroup_observations(group_name=er_io.GROUP_NAME)
     relocs_speed_filter = ecoscope.base.RelocsSpeedFilter(max_speed_kmhr=8)
-    relocs_after_filter = movbank_relocations.apply_reloc_filter(relocs_speed_filter)
+    relocs_after_filter = relocations.apply_reloc_filter(relocs_speed_filter)
     relocs_after_filter.remove_filtered(inplace=True)
-    assert movbank_relocations.shape[0] != relocs_after_filter.shape[0]
+    assert relocations.shape[0] != relocs_after_filter.shape[0]
 
 
-def test_relocs_distancefilter(movbank_relocations):
+def test_relocs_distancefilter(er_io):
+    relocations = er_io.get_subjectgroup_observations(group_name=er_io.GROUP_NAME)
     relocs_speed_filter = ecoscope.base.RelocsDistFilter(min_dist_km=1.0, max_dist_km=6.0)
-    relocs_after_filter = movbank_relocations.apply_reloc_filter(relocs_speed_filter)
+    relocs_after_filter = relocations.apply_reloc_filter(relocs_speed_filter)
     relocs_after_filter.remove_filtered(inplace=True)
-    assert movbank_relocations.shape[0] != relocs_after_filter.shape[0]
+    assert relocations.shape[0] != relocs_after_filter.shape[0]
 
 
-def test_relocations_from_gdf_preserve_fields(movbank_relocations):
-    gpd.testing.assert_geodataframe_equal(movbank_relocations, ecoscope.base.Relocations.from_gdf(movbank_relocations))
+def test_relocations_from_gdf_preserve_fields(er_io):
+    relocations = er_io.get_subjectgroup_observations(group_name=er_io.GROUP_NAME)
+    gpd.testing.assert_geodataframe_equal(relocations, ecoscope.base.Relocations.from_gdf(relocations))
 
 
-def test_displacement_property(movbank_relocations):
-    trajectory = ecoscope.base.Trajectory.from_relocations(movbank_relocations)
+def test_displacement_property(er_io):
+    relocations = er_io.get_subjectgroup_observations(group_name=er_io.GROUP_NAME)
+    trajectory = ecoscope.base.Trajectory.from_relocations(relocations)
     expected = pd.Series(
         [2633.760505, 147749.545621],
         index=pd.Index(["Habiba", "Salif Keita"], name="groupby_col"),
@@ -46,8 +51,9 @@ def test_displacement_property(movbank_relocations):
     )
 
 
-def test_tortuosity(movbank_relocations):
-    trajectory = ecoscope.base.Trajectory.from_relocations(movbank_relocations)
+def test_tortuosity(er_io):
+    relocations = er_io.get_subjectgroup_observations(group_name=er_io.GROUP_NAME)
+    trajectory = ecoscope.base.Trajectory.from_relocations(relocations)
     expected = pd.Series(
         [51.65388458528601, 75.96149479123005],
         index=pd.Index(["Habiba", "Salif Keita"], name="groupby_col"),
@@ -58,8 +64,9 @@ def test_tortuosity(movbank_relocations):
     )
 
 
-def test_turn_angle(movbank_relocations):
-    trajectory = ecoscope.base.Trajectory.from_relocations(movbank_relocations)
+def test_turn_angle(er_io):
+    relocations = er_io.get_subjectgroup_observations(group_name=er_io.GROUP_NAME)
+    trajectory = ecoscope.base.Trajectory.from_relocations(relocations)
     trajectory = trajectory.loc[trajectory.groupby_col == "Habiba"].head(5)
     trajectory["heading"] = [0, 90, 120, 60, 300]
     turn_angle = trajectory.get_turn_angle()
@@ -85,7 +92,8 @@ def test_turn_angle(movbank_relocations):
     pandas.testing.assert_series_equal(turn_angle, expected)
 
 
-def test_sampling(movbank_relocations):
+def test_sampling(er_io):
+    relocations = er_io.get_subjectgroup_observations(group_name=er_io.GROUP_NAME)
     relocs_1 = ecoscope.base.Relocations.from_gdf(
         gpd.GeoDataFrame(
             {"fixtime": pd.date_range(0, periods=1000, freq="1S", tz="utc")},
@@ -113,11 +121,11 @@ def test_sampling(movbank_relocations):
         max_y=18,
         filter_point_coords=[[180, 90], [0, 0]],
     )
-    movbank_relocations.apply_reloc_filter(pnts_filter, inplace=True)
-    movbank_relocations.remove_filtered(inplace=True)
-    movbank_trajectory = ecoscope.base.Trajectory.from_relocations(movbank_relocations)
-    downsampled_relocs_noint = movbank_trajectory.downsample("10800S", tolerance="900S")
-    downsampled_relocs_int = movbank_trajectory.downsample("10800S", interpolation=True)
+    relocations.apply_reloc_filter(pnts_filter, inplace=True)
+    relocations.remove_filtered(inplace=True)
+    trajectory = ecoscope.base.Trajectory.from_relocations(relocations)
+    downsampled_relocs_noint = trajectory.downsample("10800S", tolerance="900S")
+    downsampled_relocs_int = trajectory.downsample("10800S", interpolation=True)
 
     expected_noncontiguous_1 = gpd.read_feather("tests/test_output/upsampled_noncontiguous_1.feather")
     expected_noncontiguous_2 = gpd.read_feather("tests/test_output/upsampled_noncontiguous_2.feather")
@@ -131,8 +139,9 @@ def test_sampling(movbank_relocations):
 
 
 @pytest.mark.filterwarnings("ignore:Target with index", 'ignore: ERFA function "utctai"')
-def test_daynight_ratio(movbank_relocations):
-    trajectory = ecoscope.base.Trajectory.from_relocations(movbank_relocations)
+def test_daynight_ratio(er_io):
+    relocations = er_io.get_subjectgroup_observations(group_name=er_io.GROUP_NAME)
+    trajectory = ecoscope.base.Trajectory.from_relocations(relocations)
     expected = pd.Series(
         [
             2.212816,
