dd4f05dce1c04c129acb70bccfbcbf4b243e0bcf
mdevz-22 <giuliana.castellini@nimblegravity.com>
2023-08-18 13:12:00 -0300

    extend-control-element-class

--- tests/test_ng.py
+++ tests/test_ng.py
@@ -4,4 +4,4 @@ from ecoscope.mapping.map import EcoMap
 
 m=EcoMap(draw_control=False)
 m.add_north_arrow(position="topleft",angle=0,scale=1.0)
-m.to_png(r'C:\Users\DELL\Documents\EcoMap\new.png')
+m.to_html(r'C:\Users\DELL\Documents\EcoMap\new.html')
