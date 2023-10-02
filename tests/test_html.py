e22fd7b3692fd62169340b2e1379105ebbec63dc
Eric <gitonga@gmail.com>
2023-10-02 23:32:51 +0300

    revert to selenium

--- tests/test_html.py
+++ tests/test_html.py
@@ -4,8 +4,7 @@ from ecoscope.mapping.map import EcoMap
 
 sys.path.append("../ecoscope")
 
-
 m = EcoMap(draw_control=False)
 
-relative_path = os.path.join("tests/test_output/png_map.png")
-m.to_png(relative_path)
+relative_path = os.path.join("tests/test_output/new.html")
+m.to_html(relative_path)
