205050faaf7c7cfb37362048379a6d4faa439301
Eric <gitonga@gmail.com>
2024-01-09 16:39:39 +0300

    update based on fixing north-arrow bug

--- tests/test_ng.py
+++ tests/test_ng.py
@@ -1,10 +1,11 @@
 import sys
 import os
-sys.path.append('../ecoscope')
+
+sys.path.append("../ecoscope")
 from ecoscope.mapping.map import EcoMap
 
-m=EcoMap(draw_control=False)
-m.add_north_arrow(position="topleft",angle=0,scale=1.0)
+m = EcoMap(draw_control=False)
+m.add_north_arrow(position="topleft", angle=0, scale=1.0)
 
-relative_path=os.path.join('tests/test_output/new.html')
+relative_path = os.path.join("tests/test_output/new.html")
 m.to_html(relative_path)
