d067c79f45fd6c5ff9c93d439dc4b6cb7d23e739
Alex Morling <atmorling@gmail.com>
2024-04-09 11:18:02 +0200

    Fix some failing tests

--- tests/test_seasons.py
+++ tests/test_seasons.py
@@ -10,14 +10,16 @@ if not pytest.earthengine:
     )
 
 
-@pytest.mark.skip(reason="this has been failing since May 2022; will be fixed in a follow-up pull")
+@pytest.mark.skipif(not pytest.earthengine, reason="No connection to EarthEngine.")
 def test_seasons():
     gdf = gpd.read_file("tests/sample_data/vector/AOI_sites.gpkg").to_crs(4326)
 
     aoi = gdf.geometry.iat[0]
 
     # Extract the standardized NDVI ndvi_vals within the AOI
-    ndvi_vals = ecoscope.analysis.seasons.std_ndvi_vals(aoi, start="2010-01-01", end="2021-01-01")
+    ndvi_vals = ecoscope.analysis.seasons.std_ndvi_vals(
+        aoi, img_coll="MODIS/061/MOD13A1", band="NDVI", start="2010-01-01", end="2021-01-01"
+    )
 
     # Calculate the seasonal transition point
     cuts = ecoscope.analysis.seasons.val_cuts(ndvi_vals, 2)
