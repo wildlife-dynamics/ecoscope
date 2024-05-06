0637ad4d07dda3527a8329579bec0db387061977
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-05-06 15:25:39 -0700

    test continued

--- tests/distributed/test_custom_types.py
+++ tests/distributed/test_custom_types.py
@@ -16,30 +16,23 @@ def df_with_parquet_path(tmp_path) -> tuple[pd.DataFrame, str]:
     return df, path.as_posix()
 
 
-def get_df(df: edt.InputDataframe):
-    return df
-
-
-def test_dataframe_type(df_with_parquet_path: tuple[pd.DataFrame, str]):
+def test_InputDataframe_coercion(df_with_parquet_path: tuple[pd.DataFrame, str]):
     df, parquet_path = df_with_parquet_path
 
-    # obviously if we pass through a df, we get a df
+    def get_df(df: edt.InputDataframe):
+        return df
+
+    # without coercion: pass a df, get a df; pass a str, get a str
     df_passthrough = get_df(df)
     assert isinstance(df_passthrough, pd.DataFrame)
-    
-    # ...and without coercion, we get a str if we give a str
     df_from_path_no_coercion = get_df(parquet_path)
     assert isinstance(df_from_path_no_coercion, str)
 
-    # but if we wrap `get_df` in pydantic validation
+    # with coercion: pass a path to a parquet file, get a df back
     get_df_with_coercion: Callable = validate_call(
-        get_df,
-        # let's pydantic accept pandas dataframe as type
-        config={"arbitrary_types_allowed": True},
+        get_df, config={"arbitrary_types_allowed": True},
     )
-    # ...and pass it a path to a parquet dataset...
     df_from_path_with_coercion = get_df_with_coercion(parquet_path)
-    # then the `Dataframe` type auto-magically loads the df
     assert isinstance(df_from_path_with_coercion, pd.DataFrame)
 
     # and it round-trips
@@ -47,7 +40,8 @@ def test_dataframe_type(df_with_parquet_path: tuple[pd.DataFrame, str]):
 
 
 # TODO: test BaseModel generation for frontend schema
-# TODO: Results model
+# TODO: Results model -> i.e. **validate/serialize return valies**
 # TODO: hardware/metal
 # TODO: pandera
 # TODO: importable with minimal dependencies
+# TODO: notebook example
