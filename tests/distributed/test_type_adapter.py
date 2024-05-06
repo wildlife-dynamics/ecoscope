ddf64ebd618420e63165ee88d08475daf3007bbd
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-05-06 16:21:30 -0700

    nontrivial example passes except description

--- tests/distributed/test_type_adapter.py
+++ tests/distributed/test_type_adapter.py
@@ -1,8 +1,10 @@
-import pytest
+from typing import Annotated
 
-from pydantic import BaseModel, TypeAdapter
+import numpy as np
+from pydantic import BaseModel, ConfigDict, Field, TypeAdapter
 from pydantic.json_schema import GenerateJsonSchema
 
+import ecoscope.distributed.types as etd
 
 class MatchingSchema(GenerateJsonSchema):
     def generate(self, schema, mode='validation'):
@@ -29,3 +31,45 @@ def test_jsonschema_from_signature_basic():
     from_func = TypeAdapter(func).json_schema(schema_generator=MatchingSchema)
     from_model = FuncSignature.model_json_schema(schema_generator=MatchingSchema)
     assert from_func == from_model
+
+
+def test_jsonschema_from_signature_nontrivial():
+    config_dict = ConfigDict(arbitrary_types_allowed=True)
+
+    class TimeDensityConfig(BaseModel):
+        model_config = config_dict
+
+        input_df: etd.InputDataframe
+        pixel_size: Annotated[
+            float,
+            Field(default=250.0) # , description="Pixel size for raster profile."),
+        ]
+        crs: Annotated[str, Field(default="ESRI:102022")]
+        nodata_value: Annotated[float, Field(default=float("nan"), allow_inf_nan=True)]
+        band_count: Annotated[int, Field(default=1)]
+        max_speed_factor: Annotated[float, Field(default=1.05)]
+        expansion_factor: Annotated[float, Field(default=1.3)]
+        percentiles: Annotated[list[float], Field(default=[50.0, 60.0, 70.0, 80.0, 90.0, 95.0])]
+
+    def calculate_time_density(
+        input_df: etd.InputDataframe,
+        pixel_size: Annotated[
+            float,
+            Field(default=250.0)  # , description="Pixel size for raster profile."),
+        ],
+        crs: Annotated[str, Field(default="ESRI:102022")],
+        nodata_value: Annotated[float, Field(default=float("nan"), allow_inf_nan=True)],
+        band_count: Annotated[int, Field(default=1)],
+        max_speed_factor: Annotated[float, Field(default=1.05)],
+        expansion_factor: Annotated[float, Field(default=1.3)],
+        percentiles: Annotated[list[float], Field(default=[50.0, 60.0, 70.0, 80.0, 90.0, 95.0])],
+    ): ...
+
+    schema_kws = dict(schema_generator=MatchingSchema)
+    from_func = TypeAdapter(
+        calculate_time_density,
+        config=config_dict,
+    ).json_schema(**schema_kws)
+    from_model = TimeDensityConfig.model_json_schema(**schema_kws)
+    # `nodata_value` defaults to `nan`; numpy evals `nan == nan` as true
+    np.testing.assert_equal(from_func, from_model)
