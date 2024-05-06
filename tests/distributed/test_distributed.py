09d06e3fd3bc92b085ed7b1a591ecdd4b18a257d
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-05-06 15:09:08 -0700

    test fixture

--- tests/distributed/test_distributed.py
+++ tests/distributed/test_distributed.py
@@ -1,39 +1,53 @@
 import pathlib
+from typing import Callable
 
 import pandas as pd
+import pytest
 from pydantic import validate_call
 
-from ecoscope.distributed.types import Dataframe
+import ecoscope.distributed.types as edt
 
 
-def get_df(df: Dataframe):
+@pytest.fixture
+def df_with_parquet_path(tmp_path) -> tuple[pd.DataFrame, str]:
+    path: pathlib.Path = tmp_path / "df.parquet"
+    df = pd.DataFrame(data={'col1': [1, 2], 'col2': [3, 4]})
+    df.to_parquet(path)     
+    return df, path.as_posix()
+
+
+def get_df(df: edt.InputDataframe):
     return df
 
 
-def test_dataframe_type(tmp_path):
-    path: pathlib.Path = tmp_path / "df.parquet"
-
-    df = pd.DataFrame(data={'col1': [1, 2], 'col2': [3, 4]})
-    df.to_parquet(path)  
+def test_dataframe_type(df_with_parquet_path: tuple[pd.DataFrame, str]):
+    df, parquet_path = df_with_parquet_path
 
     # obviously if we pass through a df, we get a df
-    df_fromdf_novalidate = get_df(df)
-    assert isinstance(df_fromdf_novalidate, pd.DataFrame)
+    df_passthrough = get_df(df)
+    assert isinstance(df_passthrough, pd.DataFrame)
     
     # ...and without coercion, we get a str if we give a str
-    df_fromstr_novalidate = get_df(path.as_posix())
-    assert isinstance(df_fromstr_novalidate, str)
+    df_from_path_no_coercion = get_df(parquet_path)
+    assert isinstance(df_from_path_no_coercion, str)
 
     # but if we wrap `get_df` in pydantic validation
-    validated_get_df = validate_call(
+    get_df_with_coercion: Callable = validate_call(
         get_df,
         # let's pydantic accept pandas dataframe as type
-        config={"arbitrary_types_allowed": True}
+        config={"arbitrary_types_allowed": True},
     )
     # ...and pass it a path to a parquet dataset...
-    df_fromstr_validate = validated_get_df(path.as_posix())
+    df_from_path_with_coercion = get_df_with_coercion(parquet_path)
     # then the `Dataframe` type auto-magically loads the df
-    assert isinstance(df_fromstr_validate, pd.DataFrame)
+    assert isinstance(df_from_path_with_coercion, pd.DataFrame)
 
     # and it round-trips
-    pd.testing.assert_frame_equal(df, df_fromstr_validate)
+    pd.testing.assert_frame_equal(df, df_from_path_with_coercion)
+
+
+# TODO: test BaseModel generation for frontend schema
+# TODO: Results model
+# TODO: hardware/metal
+# TODO: pandera
+# TODO: importable with minimal dependencies
