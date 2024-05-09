28a8ac1b518d440edaef6c4755795315edd5a2b9
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-05-08 17:38:25 -0700

    field renaming

--- ecoscope/distributed/decorators.py
+++ ecoscope/distributed/decorators.py
@@ -1,19 +1,23 @@
 from dataclasses import dataclass, replace
-from typing import Callable
+from typing import Callable, NewType
 
 import functools
+
+ArgName = NewType("ArgName", str)
 
 
 @dataclass
 class distributed:
     func: Callable
-    serializer: Callable | None = None
-    deserializer: Callable | None = None
+    arg_loaders: dict[ArgName, Callable] | None = None
+    dump_return: Callable | None = None
+    # TODO memoize: bool
+    # TODO arg_hashers: dict (for memoize) - need to be stable across processes
 
     def __post_init__(self):
         functools.update_wrapper(self, self.func)
 
-    def override(self, changes: dict):
+    def override(self, changes: dict) -> "distributed":
         return replace(self, changes)
 
     def __call__(self, *args, **kwargs):
