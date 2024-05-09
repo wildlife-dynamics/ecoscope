934d84ce2b318f99d309bd1379b9a4ea0ca47935
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-05-09 16:21:33 -0700

    move prevalidator assignment into __post_init__

--- ecoscope/distributed/decorators.py
+++ ecoscope/distributed/decorators.py
@@ -1,35 +1,12 @@
 import functools
+import types
 from dataclasses import dataclass, field, replace
 from typing import Callable, NewType
 
+from pydantic import validate_call
 from pydantic.functional_validators import BeforeValidator
 
 ArgName = NewType("ArgName", str)
-
-
-def assign_arg_prevalidators(
-    func: Callable,
-    arg_prevalidators: dict[ArgName, Callable],
-) -> None:
-    """Changes input `func.__annotations__` in-place."""
-    # NOTE: copy.deepcopy(func) preserves references to the initial func's __annotations__
-    # there are ways to avoid this but for now it seems easier to just mutate in-place.
-    # If that causes problems, we can change this to return a true deep copy in the future.
-
-    for arg_name in arg_prevalidators:
-        # assumes there is an annotation and that is of type typing.Annotated
-        # TODO: handle case of no annotation, or non-Annotated annotation
-        meta = list(func.__annotations__[arg_name].__metadata__)
-        # if there are is an existing BeforeValidator instance in the metadata,
-        # we will overwrite it by re-assigning to its index. if not, we will just
-        # add our new BeforeValidator to the end of the list (i.e., index it as -1)
-        bf_idx = (
-            -1
-            if not any([isinstance(m, BeforeValidator) for m in meta])
-            else [i for i, m in enumerate(meta) if isinstance(m, BeforeValidator)][0]
-        )
-        meta[bf_idx] = BeforeValidator(func=arg_prevalidators[arg_name])
-        func.__annotations__[arg_name].__metadata__ = tuple(meta)
 
 
 @dataclass
@@ -42,30 +19,41 @@ class distributed:
     arg_prevalidators : dict[ArgName, Callable]
         ...
     """
-    func: Callable
+    func: types.FunctionType
     arg_prevalidators: dict[ArgName, Callable] = field(default_factory=dict)
     return_postvalidator: Callable | None = None
+    validate: bool = False
 
     def __post_init__(self):
-        functools.update_wrapper(self, self.func)
-        # NOTE: the validation steps described below do suggest BaseModel could be useful,
-        # but because this class does not need to be serialized or deserialized, and is not
-        # designed to directly handle user input, i do think keeping the implementation as
-        # lightweight as possible is desirable.
-
         # TODO: make sure the keys of arg_prevalidators are all arg_names on self.func
         # TODO: `strict=True` requires the callable values of arg_prevalidators to be
         # hinted with a return type, and for the return type of the callable to match
         # the input type of the matching arg on self.func
-
+        for arg_name in self.arg_prevalidators:
+            # assumes there is an annotation and that is of type typing.Annotated
+            # TODO: handle case of no annotation, or non-Annotated annotation
+            meta = list(self.func.__annotations__[arg_name].__metadata__)
+            # if there are is an existing BeforeValidator instance in the metadata,
+            # we will overwrite it by re-assigning to its index. if not, we will just
+            # add our new BeforeValidator to the end of the list (i.e., index it as -1)
+            bf_idx = (
+                -1
+                if not any([isinstance(m, BeforeValidator) for m in meta])
+                else [i for i, m in enumerate(meta) if isinstance(m, BeforeValidator)][0]
+            )
+            meta[bf_idx] = BeforeValidator(func=self.arg_prevalidators[arg_name])
+            self.func.__annotations__[arg_name].__metadata__ = tuple(meta)
+        if self.validate:
+            self.func = validate_call(self.func, validate_return=True)
         # TODO: make sure return_postvalidator is a single-argument callable
         # TODO: `strict=True` requires return_postvalidator to be type-hinted and for
         # the type of it's single argument to be the same as the return type of self.func
+        functools.update_wrapper(self, self.func)
+
+    # TODO: disable setters for arg_prevalidators and return_postvalidator
 
     def replace(self, /, **changes: dict) -> "distributed":
         return replace(self, **changes)
 
     def __call__(self, *args, **kwargs):
-        # TODO: apply arg_prevalidators and arg_postvalidators to self.func annotations
-
         return self.func(*args, **kwargs)
--- tests/distributed/test_decorators.py
+++ tests/distributed/test_decorators.py
@@ -2,49 +2,42 @@ from typing import Annotated
 
 from pydantic.functional_validators import BeforeValidator
 
-from ecoscope.distributed.decorators import distributed, assign_arg_prevalidators
-
-
-def f(a: int, b: int) -> int:
-    return a + b
+from ecoscope.distributed.decorators import distributed
 
 
 def test_call_simple():
+    def f(a: int, b: int) -> int:
+        return a + b
+
     d = distributed(f)
     assert f(1, 2) == 3
     assert d(1, 2) == 3
 
-
-def test_replace():
-    d0 = distributed(f)
-    assert d0.arg_prevalidators == {}
-    assert d0(1, 2) == 3
-
-    arg_prevalidators = {"a": lambda x: int(x)}
-    d1 = d0.replace(arg_prevalidators=arg_prevalidators)
-    assert d1.arg_prevalidators == arg_prevalidators
-    # although we assigned arg_prevalidators, this is not
-    # testing that they *work*, just that we can assign them
-    assert d1(1, 2) == 3
+    @distributed
+    def f(a: int, b: int) -> int:
+        return a + b
+    
+    assert d.func(1, 2) == 3
+    assert d(1, 2) == 3
 
 
-def test_assign_arg_prevalidators():
-    def initial_prevalidator(x):
-        return x
-
-    def f(a: Annotated[int, BeforeValidator(initial_prevalidator)]) -> int:
+def test_call_with_arg_prevalidators():
+    @distributed
+    def f(a: Annotated[int, "some metadata field"]) -> int:
         return a
-
-    f_meta: list[BeforeValidator] = f.__annotations__["a"].__metadata__
-    assert initial_prevalidator(1) == 1
-    assert f_meta[0].func(1) == 1
-
-    def new_prevalidator(x):
+    
+    def a_prevalidator(x):
         return x + 1
 
-    arg_prevalidators = {"a": new_prevalidator}
-    assign_arg_prevalidators(f, arg_prevalidators)
-
-    f_meta: list[BeforeValidator] = f.__annotations__["a"].__metadata__
-    assert new_prevalidator(1) == 2
-    assert f_meta[0].func(1) == 2
+    # FIXME: __annotations__ is a shared reference between `f` and `f_new`, even though
+    # f_new is a different object. this `.replace` call _should_ give us a totally different
+    # `.func` attribute, but it doesn't, because of that shared reference.
+    f_new = f.replace(arg_prevalidators={"a": a_prevalidator})
+    # FIXME: this assert should pass, but it does not:
+    # assert f_new.func.__annotations__ != f.func.__annotations__
+    f_func_meta: list[BeforeValidator] = f_new.func.__annotations__["a"].__metadata__
+    assert a_prevalidator(1) == 2
+    assert f_func_meta[0].func(1) == 2  # calling prevalidator directly, we see its behavior
+    assert f(1) == 1   # but without `validate=True` we still get normal behavior
+    # only when we set validate=True do we finally see the prevalidator is invoked
+    assert f.replace(validate=True)(1) == 2
