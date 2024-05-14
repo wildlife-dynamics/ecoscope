2a0aa794cd46b11b4415d921cb5741a72614a3ae
Alex Morling <atmorling@gmail.com>
2024-05-14 16:40:58 +0200

    Add test for proximity.py

---
+++ tests/test_proiximity.py
@@ -0,0 +1,18 @@
+from ecoscope.analysis.proximity import SpatialFeature, Proximity, ProximityProfile
+from ecoscope.base import Trajectory
+
+
+def test_proximity(er_io):
+    er_features = er_io.get_spatial_features_group(spatial_features_group_id="15698426-7e0f-41df-9bc3-495d87e2e097")
+
+    prox_profile = ProximityProfile([])
+
+    for row in er_features.iterrows():
+        prox_profile.spatial_features.append(SpatialFeature(row[1]["name"], row[1]["pk"], row[1]["geometry"]))
+
+    relocations = er_io.get_subjectgroup_observations(group_name=er_io.GROUP_NAME)
+    trajectory = Trajectory.from_relocations(relocations)
+
+    proximity_events = Proximity.calculate_proximity(proximity_profile=prox_profile, trajectory=trajectory)
+
+    assert len(proximity_events["spatialfeature_id"].unique()) == len(prox_profile.spatial_features)
