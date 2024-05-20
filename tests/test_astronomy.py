b7eca72d9d835fc8f03c0db5faf54e5ca6f03292
Alex Morling <atmorling@gmail.com>
2024-05-20 15:53:58 +0200

    Fix a comment

--- tests/test_astronomy.py
+++ tests/test_astronomy.py
@@ -15,7 +15,7 @@ def test_to_EarthLocation(movebank_relocations):
         proj_to="+proj=geocent +ellps=WGS84 +datum=WGS84 +units=m +no_defs",
     )
 
-    # Check the projected values in the
+    # Check the projected values in the returned EarthLocation are what we expect
     test_val = transform.transform(xx=test_point.x, yy=test_point.y, zz=0)
     assert test_val[0] == transformed[0].x.value
     assert test_val[1] == transformed[0].y.value
