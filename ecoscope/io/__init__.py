397f6b48a726d6bcd0e68f83489bf091ef64ca38
Alex Morling <atmorling@gmail.com>
2024-08-02 10:05:19 +0200

    make async earthranger client optional

--- ecoscope/io/__init__.py
+++ ecoscope/io/__init__.py
@@ -1,15 +1,29 @@
 from ecoscope.io import earthranger, eetools, raster, utils
 from ecoscope.io.earthranger import EarthRangerIO
-from ecoscope.io.async_earthranger import AsyncEarthRangerIO
 from ecoscope.io.utils import download_file
 
 __all__ = [
     "earthranger",
     "EarthRangerIO",
-    "AsyncEarthRangerIO",
     "download_file",
     "earthranger_utils",
     "eetools",
     "raster",
     "utils",
 ]
+
+try:
+    from ecoscope.io.async_earthranger import AsyncEarthRangerIO
+
+    __all__ = [
+        "earthranger",
+        "EarthRangerIO",
+        "AsyncEarthRangerIO",
+        "download_file",
+        "earthranger_utils",
+        "eetools",
+        "raster",
+        "utils",
+    ]
+except ImportError:
+    pass
