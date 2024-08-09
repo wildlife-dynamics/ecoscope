493cc53ca427016fef73ea04257198f7b348cc7c
davidovichmarcos <davidovich.marcos@gmail.com>
2024-08-09 10:11:38 -0300

    test pipeline remotely

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -9,16 +9,15 @@ NB_DIR = pathlib.Path(__file__).parent.parent / "doc" / "source" / "notebooks"
 
 KNOWN_ERRORS_REGEXES = {  # This is basically a GitHub ticket queue
     "EarthRanger_IO.ipynb": "Series found",
-    "GEE_IO.ipynb": "Google Earth Engine API has not been used in project",
     "Relocations_and_Trajectories.ipynb": "not a zip file",
     "EcoGraph.ipynb": "not a zip file",
-    "EcoMap.ipynb": "Google Earth Engine API has not been used in project",
     "EcoPlot.ipynb": "not a zip file",
-    "Anomaly.ipynb": "Google Earth Engine API has not been used in project",
-    "Calculation.ipynb": "Google Earth Engine API has not been used in project",
-    "Chart.ipynb": "not a zip file",
+    "Landscape Grid.ipynb": "not a zip file",
+    "Seasonal Calculation.ipynb": "buffer source array is read-only",
+    "Tracking Data Gantt Chart.ipynb": "not a zip file",
 }
-
+class UnexecptedNotebookExecutionError(Exception):
+    pass
 
 @dataclass
 class Notebook:
@@ -49,14 +48,7 @@ def test_notebooks(notebook: Notebook):
         if notebook.raises:  # these are the ones we expect to raise errors
             pytest.xfail(f"Notebook {notebook.path} is known to fail with error {notebook.raises_match}")
         else:
-            print(e.__traceback__)
-            raise e  # this is not an expected error, so re-raise the captured error and make the test FAIL
-    # with pytest.raises(Exception, match=notebook.raises_match):
-            # First prove to ourselves that the notebook is still broken
-            # then xfail when we know it is
-        # Option 2: if papermill run in "capture errors" mode
-        # error = papermill.execute_notebook(str(notebook), str(notebook.with_name("output.ipynb")))
-        # assert notebook.expected_error_cell_number == error.cell_number
-        # assert notebook_raises_match == error.text
-        # pytest.xfail(f"Notebook {notebook.path} is known to fail with error {notebook.raises_match}")
-    # papermill.execute_notebook(str(notebook), str(notebook.with_name("output.ipynb")))
+            raise UnexecptedNotebookExecutionError(
+                f"{notebook.path.name=} not in {list(KNOWN_ERRORS_REGEXES)= } but execution errored. "
+                "This notebook is unexpectedly broken."
+            ) from e
