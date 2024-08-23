fef0e5054e91597a977436d4cb380d65c929efad
Alex Morling <atmorling@gmail.com>
2024-08-23 15:53:59 +0200

    adding trajectories, settling on structure, feeling good about this

--- ecoscope/base/new_base.py
+++ ecoscope/base/new_base.py
@@ -4,7 +4,7 @@ import geopandas as gpd
 import numpy as np
 import pandas as pd
 
-# import shapely
+import shapely
 from pyproj import Geod
 
 from ecoscope.base._dataclasses import (
@@ -12,37 +12,21 @@ from ecoscope.base._dataclasses import (
     RelocsDateRangeFilter,
     RelocsDistFilter,
     RelocsSpeedFilter,
-    # TrajSegFilter,
+    TrajSegFilter,
 )
 from functools import cached_property
 
 
-@pd.api.extensions.register_dataframe_accessor("relocations")
-class NewRelocations:
-    """
-    Relocation is a model for a set of fixes from a given subject.
-    Because fixes are temporal, they can be ordered asc or desc. The additional_data dict can contain info
-    specific to the subject and relocations: name, type, region, sex etc. These values are applicable to all
-    fixes in the relocations array. If they vary, then they should be put into each fix's additional_data dict.
-    """
-
-    def __init__(self, pandas_obj):
-        self._validate(pandas_obj)
-        self.gdf = pandas_obj
-
-    @staticmethod
-    def _validate(obj):
-        assert isinstance(obj, gpd.GeoDataFrame)
-
+class StraighttrackMixin:
     @property
     def start_fixes(self):
         # unpack xy-coordinates of start fixes
-        return self.gdf["geometry"].x, self.gdf["geometry"].y
+        return self._gdf["geometry"].x, self._gdf["geometry"].y
 
     @property
     def end_fixes(self):
         # unpack xy-coordinates of end fixes
-        return self.gdf["_geometry"].x, self.gdf["_geometry"].y
+        return self._gdf["_geometry"].x, self._gdf["_geometry"].y
 
     @property
     def inverse_transformation(self):
@@ -63,11 +47,29 @@ class NewRelocations:
 
     @property
     def timespan_seconds(self):
-        return (self.gdf["_fixtime"] - self.gdf["fixtime"]).dt.total_seconds()
+        return (self._gdf["_fixtime"] - self._gdf["fixtime"]).dt.total_seconds()
 
     @property
     def speed_kmhr(self):
         return (self.dist_meters / self.timespan_seconds) * 3.6
+
+
+@pd.api.extensions.register_dataframe_accessor("relocations")
+class NewRelocations(StraighttrackMixin):
+    """
+    Relocation is a model for a set of fixes from a given subject.
+    Because fixes are temporal, they can be ordered asc or desc. The additional_data dict can contain info
+    specific to the subject and relocations: name, type, region, sex etc. These values are applicable to all
+    fixes in the relocations array. If they vary, then they should be put into each fix's additional_data dict.
+    """
+
+    def __init__(self, pandas_obj):
+        self._validate(pandas_obj)
+        self._gdf = pandas_obj
+
+    @staticmethod
+    def _validate(obj):
+        assert isinstance(obj, gpd.GeoDataFrame)
 
     def from_gdf(self, groupby_col=None, time_col="fixtime", uuid_col=None, **kwargs):
         """
@@ -87,42 +89,44 @@ class NewRelocations:
         #     gdf = gdf.copy()
 
         if groupby_col is None:
-            if "groupby_col" not in self.gdf:
-                self.gdf["groupby_col"] = 0
+            if "groupby_col" not in self._gdf:
+                self._gdf["groupby_col"] = 0
         else:
-            self.gdf["groupby_col"] = self.gdf.loc[:, groupby_col]
+            self._gdf["groupby_col"] = self._gdf.loc[:, groupby_col]
 
         if time_col != "fixtime":
-            self.gdf["fixtime"] = self.gdf.loc[:, time_col]
+            self._gdf["fixtime"] = self._gdf.loc[:, time_col]
 
-        if not pd.api.types.is_datetime64_any_dtype(self.gdf["fixtime"]):
+        if not pd.api.types.is_datetime64_any_dtype(self._gdf["fixtime"]):
             warnings.warn(
                 f"{time_col} is not of type datetime64. Attempting to automatically infer format and timezone. "
                 "Results may be incorrect."
             )
-            self.gdf["fixtime"] = pd.to_datetime(self.gdf["fixtime"])
+            self._gdf["fixtime"] = pd.to_datetime(self._gdf["fixtime"])
 
-        if self.gdf["fixtime"].dt.tz is None:
+        if self._gdf["fixtime"].dt.tz is None:
             warnings.warn(f"{time_col} is not timezone aware. Assuming datetime are in UTC.")
-            self.gdf["fixtime"] = self.gdf["fixtime"].dt.tz_localize(tz="UTC")
+            self._gdf["fixtime"] = self._gdf["fixtime"].dt.tz_localize(tz="UTC")
 
-        if self.gdf.crs is None:
+        if self._gdf.crs is None:
             warnings.warn("CRS was not set. Assuming geometries are in WGS84.")
-            self.gdf.set_crs(4326, inplace=True)
+            self._gdf.set_crs(4326, inplace=True)
 
         if uuid_col is not None:
-            self.gdf.set_index(uuid_col, drop=False, inplace=True)
+            self._gdf.set_index(uuid_col, drop=False, inplace=True)
 
-        self.gdf["junk_status"] = False
+        self._gdf["junk_status"] = False
 
         default_cols = ["groupby_col", "fixtime", "junk_status", "geometry"]
-        extra_cols = self.gdf.columns.difference(default_cols)
+        extra_cols = self._gdf.columns.difference(default_cols)
         extra_cols = extra_cols[~extra_cols.str.startswith("extra__")]
 
-        assert self.gdf.columns.intersection("extra__" + extra_cols).empty, "Column names overlap with existing `extra`"
+        assert self._gdf.columns.intersection(
+            "extra__" + extra_cols
+        ).empty, "Column names overlap with existing `extra`"
 
-        self.gdf.rename(columns=dict(zip(extra_cols, "extra__" + extra_cols)), inplace=True)
-        return self
+        self._gdf.rename(columns=dict(zip(extra_cols, "extra__" + extra_cols)), inplace=True)
+        return self._gdf
 
     @staticmethod
     def _apply_speedfilter(relocations, fix_filter):
@@ -179,56 +183,50 @@ class NewRelocations:
         result.drop(["_geometry", "_junk_status", "distance_km"], axis=1, inplace=True)
         return result
 
-    def apply_reloc_filter(self, fix_filter=None, inplace=False):
+    def apply_reloc_filter(self, fix_filter=None):
         """Apply a given filter by marking the fix junk_status based on the conditions of a filter"""
 
-        if not self.gdf["fixtime"].is_monotonic_increasing:
-            self.gdf.sort_values("fixtime", inplace=True)
-        assert self.gdf["fixtime"].is_monotonic_increasing
-
-        if inplace:
-            frame = self.gdf
-        else:
-            frame = self.gdf.copy()
+        if not self._gdf["fixtime"].is_monotonic_increasing:
+            self._gdf.sort_values("fixtime", inplace=True)
+        assert self._gdf["fixtime"].is_monotonic_increasing
 
         # Identify junk fixes based on location coordinate x,y ranges or that match specific coordinates
         if isinstance(fix_filter, RelocsCoordinateFilter):
-            frame.loc[
-                (frame["geometry"].x < fix_filter.min_x)
-                | (frame["geometry"].x > fix_filter.max_x)
-                | (frame["geometry"].y < fix_filter.min_y)
-                | (frame["geometry"].y > fix_filter.max_y)
-                | (frame["geometry"].isin(fix_filter.filter_point_coords)),
+            self._gdf.loc[
+                (self._gdf["geometry"].x < fix_filter.min_x)
+                | (self._gdf["geometry"].x > fix_filter.max_x)
+                | (self._gdf["geometry"].y < fix_filter.min_y)
+                | (self._gdf["geometry"].y > fix_filter.max_y)
+                | (self._gdf["geometry"].isin(fix_filter.filter_point_coords)),
                 "junk_status",
             ] = True
 
         # Mark fixes outside this date range as junk
         elif isinstance(fix_filter, RelocsDateRangeFilter):
             if fix_filter.start is not None:
-                frame.loc[frame["fixtime"] < fix_filter.start, "junk_status"] = True
+                self._gdf.loc[self._gdf["fixtime"] < fix_filter.start, "junk_status"] = True
 
             if fix_filter.end is not None:
-                frame.loc[frame["fixtime"] > fix_filter.end, "junk_status"] = True
+                self._gdf.loc[self._gdf["fixtime"] > fix_filter.end, "junk_status"] = True
 
         else:
-            crs = frame.crs
-            frame.to_crs(4326)
+            crs = self._gdf.crs
+            self._gdf.to_crs(4326)
             if isinstance(fix_filter, RelocsSpeedFilter):
-                frame._update_inplace(
-                    frame.groupby("groupby_col")[frame.columns]
+                self._gdf._update_inplace(
+                    self._gdf.groupby("groupby_col")[self._gdf.columns]
                     .apply(self._apply_speedfilter, fix_filter=fix_filter)
                     .droplevel(["groupby_col"])
                 )
             elif isinstance(fix_filter, RelocsDistFilter):
-                frame._update_inplace(
-                    frame.groupby("groupby_col")[frame.columns]
+                self._gdf._update_inplace(
+                    self._gdf.groupby("groupby_col")[self._gdf.columns]
                     .apply(self._apply_distfilter, fix_filter=fix_filter)
                     .droplevel(["groupby_col"])
                 )
-            frame.to_crs(crs, inplace=True)
+            self._gdf.to_crs(crs, inplace=True)
 
-        if not inplace:
-            return frame
+        return self._gdf
 
     @cached_property
     def distance_from_centroid(self):
@@ -264,3 +262,276 @@ class NewRelocations:
         distance = self.distance_from_centroid
         _filter = distance > threshold_dist_meters
         self.relocations.loc[_filter, "junk_status"] = True
+
+
+@pd.api.extensions.register_dataframe_accessor("trajectories")
+class NewTrajectory(StraighttrackMixin):
+    """
+    A trajectory represents a time-ordered collection of segments.
+    Currently only straight track segments exist.
+    It is based on an underlying relocs object that is the point representation
+    """
+
+    def __init__(self, pandas_obj):
+        self._validate(pandas_obj)
+        self._gdf = pandas_obj
+
+    @staticmethod
+    def _validate(obj):
+        assert isinstance(obj, gpd.GeoDataFrame)
+
+    def from_relocations(self, *args, **kwargs):
+        """
+        Create Trajectory class from Relocation dataframe.
+        Parameters
+        ----------
+        gdf: Geodataframe
+            Relocation geodataframe with relevant columns
+        args
+        kwargs
+        Returns
+        -------
+        Trajectory
+        """
+        # if kwargs.get("copy") is not False:
+        #     gdf = gdf.copy()
+
+        crs = self._gdf.crs
+        self._gdf.to_crs(4326, inplace=True)
+        self._gdf = (
+            self._gdf.groupby("groupby_col")[self._gdf.columns]
+            .apply(NewTrajectory._create_multitraj)
+            .droplevel(level=0)
+        )
+        self._gdf.to_crs(crs, inplace=True)
+        self._gdf.sort_values("segment_start", inplace=True)
+        return self._gdf
+
+    def get_displacement(self):
+        """
+        Get displacement in meters between first and final fixes.
+        """
+
+        if not self["segment_start"].is_monotonic_increasing:
+            self = self.sort_values("segment_start")
+
+        gs = self.geometry.iloc[[0, -1]]
+        start, end = gs.to_crs(gs.estimate_utm_crs())
+
+        return start.distance(end)
+
+    def get_tortuosity(self):
+        """
+        Get tortuosity for dataframe defined as distance traveled divided by displacement between first and final
+        points.
+        """
+
+        return self["dist_meters"].sum() / self.get_displacement()
+
+    @staticmethod
+    def _create_multitraj(df):
+        with warnings.catch_warnings():
+            """
+            Note : This warning can be removed once the version of Geopandas is updated
+            on Colab to the one that fixes this bug
+            """
+            warnings.filterwarnings("ignore", message="CRS not set for some of the concatenation inputs")
+            df["_geometry"] = df["geometry"].shift(-1)
+
+        df["_fixtime"] = df["fixtime"].shift(-1)
+        return NewTrajectory._create_trajsegments(df[:-1])
+
+    @staticmethod
+    def _create_trajsegments(gdf):
+        coords = np.column_stack(
+            (
+                np.column_stack(gdf.trajectories.start_fixes),
+                np.column_stack(gdf.trajectories.end_fixes),
+            )
+        ).reshape(gdf.shape[0], 2, 2)
+
+        df = gpd.GeoDataFrame(
+            {
+                "groupby_col": gdf.groupby_col,
+                "segment_start": gdf.fixtime,
+                "segment_end": gdf._fixtime,
+                "timespan_seconds": gdf.trajectories.timespan_seconds,
+                "dist_meters": gdf.trajectories.dist_meters,
+                "speed_kmhr": gdf.trajectories.speed_kmhr,
+                "heading": gdf.trajectories.heading,
+                "geometry": shapely.linestrings(coords),
+                "junk_status": gdf.junk_status,
+            },
+            crs=4326,
+            index=gdf.index,
+        )
+        gdf.drop(["fixtime", "_fixtime", "_geometry"], axis=1, inplace=True)
+        extra_cols = gdf.columns.difference(df.columns)
+        gdf = gdf[extra_cols]
+
+        extra_cols = extra_cols[~extra_cols.str.startswith("extra_")]
+        gdf.rename(columns=dict(zip(extra_cols, "extra__" + extra_cols)), inplace=True)
+
+        return df.join(gdf, how="left")
+
+    def apply_traj_filter(self, traj_seg_filter, inplace=False):
+        if not self._gdf["segment_start"].is_monotonic_increasing:
+            self._gdf.sort_values("segment_start", inplace=True)
+        assert self._gdf["segment_start"].is_monotonic_increasing
+
+        assert type(traj_seg_filter) is TrajSegFilter
+        self._gdf.loc[
+            (self._gdf["dist_meters"] < traj_seg_filter.min_length_meters)
+            | (self._gdf["dist_meters"] > traj_seg_filter.max_length_meters)
+            | (self._gdf["timespan_seconds"] < traj_seg_filter.min_time_secs)
+            | (self._gdf["timespan_seconds"] > traj_seg_filter.max_time_secs)
+            | (self._gdf["speed_kmhr"] < traj_seg_filter.min_speed_kmhr)
+            | (self._gdf["speed_kmhr"] > traj_seg_filter.max_speed_kmhr),
+            "junk_status",
+        ] = True
+
+        return self
+
+    def get_turn_angle(self):
+        if not self._gdf["segment_start"].is_monotonic_increasing:
+            self._gdf.sort_values("segment_start", inplace=True)
+        assert self._gdf["segment_start"].is_monotonic_increasing
+
+        def turn_angle(traj):
+            return ((traj["heading"].diff() + 540) % 360 - 180)[traj["segment_end"].shift(1) == traj["segment_start"]]
+
+        uniq = self._gdf.groupby_col.nunique()
+        angles = (
+            self._gdf.groupby("groupby_col")[self._gdf.columns].apply(turn_angle, include_groups=False).droplevel(0)
+            if uniq > 1
+            else turn_angle(self._gdf)
+        )
+
+        return angles.rename("turn_angle").reindex(self._gdf.index)
+
+    def upsample(self, freq):
+        """
+        Interpolate to create upsampled Relocations
+        Parameters
+        ----------
+        freq : str, pd.Timedelta or pd.DateOffset
+            Sampling frequency for new Relocations object
+        Returns
+        -------
+        relocs : ecoscope.base.Relocations
+        """
+
+        freq = pd.tseries.frequencies.to_offset(freq)
+
+        if not self._gdf["segment_start"].is_monotonic_increasing:
+            self._gdf.sort_values("segment_start", inplace=True)
+
+        def f(traj):
+            traj.crs = self._gdf.crs  # Lost in groupby-apply due to GeoPandas bug
+
+            times = pd.date_range(traj["segment_start"].iat[0], traj["segment_end"].iat[-1], freq=freq)
+
+            start_i = traj["segment_start"].searchsorted(times, side="right") - 1
+            end_i = traj["segment_end"].searchsorted(times, side="left")
+            valid_i = (start_i == end_i) | (times == traj["segment_start"].iloc[start_i])
+
+            traj = traj.iloc[start_i[valid_i]].reset_index(drop=True)
+            times = times[valid_i]
+
+            return gpd.GeoDataFrame(
+                {"fixtime": times},
+                geometry=shapely.line_interpolate_point(
+                    traj["geometry"].values,
+                    (times - traj["segment_start"]) / (traj["segment_end"] - traj["segment_start"]),
+                    normalized=True,
+                ),
+                crs=traj.crs,
+            )
+
+        self._gdf.relocations.from_gdf(
+            self._gdf.groupby("groupby_col")[self._gdf.columns].apply(f, include_groups=False).reset_index(level=0)
+        )
+
+    def to_relocations(self):
+        """
+        Converts a Trajectory object to a Relocations object.
+        Returns
+        -------
+        ecoscope.base.Relocations
+        """
+
+        def f(traj):
+            traj.crs = self._gdf.crs
+            points = np.concatenate([shapely.get_point(traj.geometry, 0), shapely.get_point(traj.geometry, 1)])
+            times = np.concatenate([traj["segment_start"], traj["segment_end"]])
+
+            return (
+                gpd.GeoDataFrame(
+                    {"fixtime": times},
+                    geometry=points,
+                    crs=traj.crs,
+                )
+                .drop_duplicates(subset=["fixtime"])
+                .sort_values("fixtime")
+            )
+
+        return self._gdf.relocations.from_gdf(
+            self._gdf.groupby("groupby_col")[self._gdf.columns].apply(f, include_groups=False).reset_index(drop=True)
+        )
+
+    def downsample(self, freq, tolerance="0S", interpolation=False):
+        """
+        Function to downsample relocations.
+        Parameters
+        ----------
+        freq: str, pd.Timedelta or pd.DateOffset
+            Downsampling frequency for new Relocations object
+        tolerance: str, pd.Timedelta or pd.DateOffset
+            Tolerance on the downsampling frequency
+        interpolation: bool, optional
+            If true, interpolates locations on the whole trajectory
+        Returns
+        -------
+        ecoscope.base.Relocations
+        """
+
+        if interpolation:
+            return self._gdf.upsample(freq)
+        else:
+            freq = pd.tseries.frequencies.to_offset(freq)
+            tolerance = pd.tseries.frequencies.to_offset(tolerance)
+
+            def f(relocs_ind):
+                relocs_ind.crs = self._gdf.crs
+                fixtime = relocs_ind["fixtime"]
+
+                k = 1
+                i = 0
+                n = len(relocs_ind)
+                out = np.full(n, -1)
+                out[i] = k
+                while i < (n - 1):
+                    t_min = fixtime.iloc[i] + freq - tolerance
+                    t_max = fixtime.iloc[i] + freq + tolerance
+
+                    j = i + 1
+
+                    while (j < (n - 1)) and (fixtime.iloc[j] < t_min):
+                        j += 1
+
+                    i = j
+
+                    if j == (n - 1):
+                        break
+                    elif (fixtime.iloc[j] >= t_min) and (fixtime.iloc[j] <= t_max):
+                        out[j] = k
+                    else:
+                        k += 1
+                        out[j] = k
+
+                relocs_ind["extra__burst"] = np.array(out, dtype=np.int64)
+                relocs_ind.drop(relocs_ind.loc[relocs_ind["extra__burst"] == -1].index, inplace=True)
+                return relocs_ind
+
+            relocs = self._gdf.trajectory.to_relocations()
+            return relocs.groupby("groupby_col")[relocs.columns].apply(f).reset_index(drop=True)
--- tests/test_newbase.py
+++ tests/test_newbase.py
@@ -1,11 +1,14 @@
+import pytest
+
 import geopandas as gpd
 import geopandas.testing
 import numpy as np
 import pandas as pd
-import pandas.testing
+import ecoscope
 
 
-def test_relocations_from_gdf():
+@pytest.fixture
+def movebank_gdf():
     df = pd.read_feather("tests/sample_data/vector/movebank_data.feather")
     gdf = gpd.GeoDataFrame(
         df,
@@ -13,13 +16,53 @@ def test_relocations_from_gdf():
         crs=4326,
     )
     gdf["timestamp"] = pd.to_datetime(gdf["timestamp"], utc=True)
-    new = gdf.copy().relocations.from_gdf(
+    return gdf
+
+
+def test_relocations_from_gdf(movebank_gdf):
+    new = movebank_gdf.copy().relocations.from_gdf(
         groupby_col="individual-local-identifier",
         time_col="timestamp",
         uuid_col="event-id",
     )
 
     # assert on series.values instead of series because the index is different
-    np.testing.assert_array_equal(new.gdf.index.to_series().values, gdf["event-id"].values)
-    np.testing.assert_array_equal(new.gdf["fixtime"].values, gdf["timestamp"].values)
-    np.testing.assert_array_equal(new.gdf["groupby_col"].values, gdf["individual-local-identifier"].values)
+    np.testing.assert_array_equal(new.index.to_series().values, movebank_gdf["event-id"].values)
+    np.testing.assert_array_equal(new["fixtime"].values, movebank_gdf["timestamp"].values)
+    np.testing.assert_array_equal(new["groupby_col"].values, movebank_gdf["individual-local-identifier"].values)
+    assert not new["junk_status"].all()
+
+
+def test_apply_recloc_filter(movebank_gdf):
+
+    coord_filter = ecoscope.base.RelocsCoordinateFilter(
+        min_x=-5,
+        max_x=1,
+        min_y=12,
+        max_y=18,
+        filter_point_coords=[[180, 90], [0, 0]],
+    )
+
+    new = (
+        movebank_gdf.copy()
+        .relocations.from_gdf(
+            groupby_col="individual-local-identifier",
+            time_col="timestamp",
+            uuid_col="event-id",
+        )
+        .relocations.apply_reloc_filter(coord_filter)
+    )
+    assert new["junk_status"].any()
+
+
+def test_traj_from_relocs(movebank_gdf):
+    new = (
+        movebank_gdf.copy()
+        .relocations.from_gdf(
+            groupby_col="individual-local-identifier",
+            time_col="timestamp",
+            uuid_col="event-id",
+        )
+        .trajectories.from_relocations()
+    )
+    assert "segment_start" in new.columns
