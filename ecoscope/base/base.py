288ed3d1fe404f832b9651d19ad1a10dfe51e947
Alex Morling <atmorling@gmail.com>
2024-11-20 13:32:08 +0200

    fix trajectory bugs

--- ecoscope/base/base.py
+++ ecoscope/base/base.py
@@ -381,10 +381,10 @@ class Trajectory(EcoDataFrame):
     def _create_multitraj(df):
         if len(df) == 1:
             warnings.warn(
-                f"Subject id {df['extra__subject_id']} has only one relocation "
+                f"Subject id {df.get('groupby_col')} has only one relocation "
                 "and will be excluded from trajectory creation"
             )
-            return pd.DataFrame()
+            return None
         with warnings.catch_warnings():
             """
             Note : This warning can be removed once the version of Geopandas is updated
