8bb3ba34a14ebe1ae022773789bcb7a72b6cfe33
Yun Wu <ywu910318@gmail.com>
2024-12-30 15:07:39 +0800

    Fix filter_bad_geojson

--- ecoscope/io/earthranger_utils.py
+++ ecoscope/io/earthranger_utils.py
@@ -1,9 +1,18 @@
 import typing
+
 import geopandas as gpd
 import pandas as pd
 from dateutil import parser
 
-TIME_COLS = ["time", "created_at", "updated_at", "end_time", "last_position_date", "recorded_at", "fixtime"]
+TIME_COLS = [
+    "time",
+    "created_at",
+    "updated_at",
+    "end_time",
+    "last_position_date",
+    "recorded_at",
+    "fixtime",
+]
 
 
 def clean_kwargs(addl_kwargs={}, **kwargs):
@@ -20,9 +29,9 @@ def normalize_column(df, col):
 
 def dataframe_to_dict(events):
     if isinstance(events, gpd.GeoDataFrame):
-        events["location"] = pd.DataFrame({"longitude": events.geometry.x, "latitude": events.geometry.y}).to_dict(
-            "records"
-        )
+        events["location"] = pd.DataFrame(
+            {"longitude": events.geometry.x, "latitude": events.geometry.y}
+        ).to_dict("records")
         del events["geometry"]
 
     if isinstance(events, pd.DataFrame):
@@ -31,10 +40,14 @@ def dataframe_to_dict(events):
 
 
 def to_gdf(df):
-    longitude, latitude = (0, 1) if isinstance(df["location"].iat[0], list) else ("longitude", "latitude")
+    longitude, latitude = (
+        (0, 1) if isinstance(df["location"].iat[0], list) else ("longitude", "latitude")
+    )
     return gpd.GeoDataFrame(
         df,
-        geometry=gpd.points_from_xy(df["location"].str[longitude], df["location"].str[latitude]),
+        geometry=gpd.points_from_xy(
+            df["location"].str[longitude], df["location"].str[latitude]
+        ),
         crs=4326,
     )
 
@@ -43,7 +56,11 @@ def clean_time_cols(df):
     for col in TIME_COLS:
         if col in df.columns and not pd.api.types.is_datetime64_ns_dtype(df[col]):
             # convert x is not None to pd.isna(x) is False
-            df[col] = df[col].apply(lambda x: pd.to_datetime(parser.parse(x), utc=True) if not pd.isna(x) else None)
+            df[col] = df[col].apply(
+                lambda x: pd.to_datetime(parser.parse(x), utc=True)
+                if not pd.isna(x)
+                else None
+            )
     return df
 
 
@@ -72,5 +89,7 @@ def pack_columns(dataframe: pd.DataFrame, columns: typing.List):
     return dataframe
 
 
-def filter_bad_geojson(dataframe: pd.DataFrame):
-    return dataframe[dataframe["geojson"].apply(lambda x: True if isinstance(x, dict) and x.get("geometry") else False)]
+def filter_bad_geojson(gdf: gpd.GeoDataFrame):
+    # CAVEAT: when you apply a boolean filter to a GeoDataFrame, it can sometimes return a regular DataFrame instead of preserving the GeoDataFrame type.
+    mask = gdf["geojson"].apply(lambda x: isinstance(x, dict) and x.get("geometry"))
+    return gpd.GeoDataFrame(gdf[mask], geometry="geometry")
