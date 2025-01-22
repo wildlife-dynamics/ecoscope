Fix ETD calculation error

ecoscope/analysis/UD/etd_range.py
@@ -4,15 +4,16 @@ import typing
 from dataclasses import dataclass
 
 import numpy as np
+
 from ecoscope.base import Trajectory
 from ecoscope.io import raster
 
 try:
     import numba as nb
     import scipy
-    from sklearn import neighbors
     from scipy.optimize import minimize
     from scipy.stats import weibull_min
+    from sklearn import neighbors
 except ModuleNotFoundError:
     raise ModuleNotFoundError(
         'Missing optional dependencies required by this module. \
@@ -169,8 +170,12 @@ def calculate_etd_range(
     grid_centroids[1, 0] = y_max - raster_profile.pixel_size * 0.5
 
     centroids_coords = np.dot(grid_centroids, np.mgrid[1:2, :num_columns, :num_rows].T.reshape(-1, 3, 1))
+    centroids_coords = centroids_coords.squeeze().T
 
-    tr = neighbors.KDTree(centroids_coords.squeeze().T)
+    if centroids_coords.ndim != 2:
+        centroids_coords = centroids_coords.reshape(1, -1)
+
+    tr = neighbors.KDTree(centroids_coords)
 
     del centroids_coords
 
@@ -207,7 +212,7 @@ def calculate_etd_range(
     raster_ndarray = raster_ndarray / raster_ndarray.sum()
 
     # Set the null data values
-    raster_ndarray[raster_ndarray == 0] = raster_profile.nodata_value
+    raster_ndarray[np.isnan(raster_ndarray) | (raster_ndarray == 0)] = raster_profile.nodata_value
 
     # write raster_ndarray to GeoTIFF file.
     raster.RasterPy.write(