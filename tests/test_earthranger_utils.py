9e71086e401f70909548e331290a1a63820f91e8
Alex Morling <atmorling@gmail.com>
2025-02-26 23:09:32 +1100

    remove redundant test

--- tests/test_earthranger_utils.py
+++ tests/test_earthranger_utils.py
@@ -3,7 +3,7 @@ import pytest
 import numpy as np
 import pandas as pd
 
-from ecoscope.io.earthranger_utils import clean_time_cols, geometry_from_event_geojson
+from ecoscope.io.earthranger_utils import clean_time_cols
 
 
 @pytest.fixture
@@ -51,10 +51,3 @@ def test_clean_time_cols(df_with_times):
     # check the nan separately from the array equality
     assert np.array_equal(expected_times, cleaned["time"].array[:-1])
     assert pd.isnull(cleaned["time"]["H"])
-
-
-def test_geometry_from_event_geojson(sample_events_df_with_bad_geojson, sample_patrol_events_with_bad_geojson):
-    events = geometry_from_event_geojson(sample_events_df_with_bad_geojson)
-    patrol_events = geometry_from_event_geojson(sample_patrol_events_with_bad_geojson)
-    assert len(events[events["geometry"].isna()]) == 2
-    assert len(patrol_events[patrol_events["geometry"].isna()]) == 4
