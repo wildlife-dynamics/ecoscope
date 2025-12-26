5badb52768091cba7d4309f5c26d864f7a45fbe9
Alex Morling <atmorling@gmail.com>
2025-12-26 11:33:14 +0200

    remove wip

--- ecoscope/analysis/feature_density.py
+++ ecoscope/analysis/feature_density.py
@@ -36,37 +36,3 @@ def calculate_feature_density(
     grid["density"] = grid.geometry.apply(clip_density)
     grid["density"] = grid["density"].replace(0, np.nan)  # Set 0's to nan so they don't draw on map
     return grid
-
-
-def aggregate_features(
-    selection: gpd.GeoDataFrame,
-    grid: gpd.GeoDataFrame,
-    geometry_type: Literal["point", "line"] = "point",
-    aggregate: str | None = "count",
-    aggregate_column: str | None = None,
-) -> gpd.GeoDataFrame:
-    # project selection data to the crs of the provided grid
-    selection = selection.to_crs(grid.crs)
-
-    if aggregate_column:
-        assert is_numeric_dtype(selection[aggregate_column])
-
-    def clip_density(cell):
-        if geometry_type == "point":
-            cell_mask = selection.geometry.within(cell)
-            result = selection[cell_mask]
-
-            if aggregate_column:
-                return result[aggregate_column].aggregate(aggregate)
-
-            return result[cell_mask].aggregate(aggregate)
-        elif geometry_type == "line":
-            result = selection.clip_by_rect(*cell.bounds)
-            result = result[~result.is_empty]
-            return result.geometry.length.aggregate(aggregate)
-        else:
-            raise ValueError("Unsupported geometry type")
-
-    grid["density"] = grid.geometry.apply(clip_density)
-    grid["density"] = grid["density"].replace(0, np.nan)  # Set 0's to nan so they don't draw on map
-    return grid
