cf3e5f9c6f161f192d039da93aa8fc13be3975df
Alex Morling <atmorling@gmail.com>
2025-12-26 11:25:35 +0200

    rework function

--- ecoscope/analysis/feature_density.py
+++ ecoscope/analysis/feature_density.py
@@ -9,7 +9,7 @@ def calculate_feature_density(
     selection: gpd.GeoDataFrame,
     grid: gpd.GeoDataFrame,
     geometry_type: Literal["point", "line"] = "point",
-    aggregate: str | None = "count",
+    aggregate: str | None = None,
     aggregate_column: str | None = None,
 ) -> gpd.GeoDataFrame:
     # project selection data to the crs of the provided grid
@@ -22,17 +22,16 @@ def calculate_feature_density(
         if geometry_type == "point":
             cell_mask = selection.geometry.within(cell)
             result = selection[cell_mask]
-
-            if aggregate_column:
-                return result[aggregate_column].aggregate(aggregate)
-
-            return result[cell_mask].aggregate(aggregate)
+            aggregate_func = aggregate if aggregate else "count"
+            series_to_aggregate = result[aggregate_column] if aggregate_column else result[cell_mask]
         elif geometry_type == "line":
             result = selection.clip_by_rect(*cell.bounds)
             result = result[~result.is_empty]
-            return result.geometry.length.aggregate(aggregate)
+            aggregate_func = aggregate if aggregate else "sum"
+            series_to_aggregate = result[aggregate_column] if aggregate_column else result.geometry.length
         else:
             raise ValueError("Unsupported geometry type")
+        return series_to_aggregate.aggregate(aggregate_func)
 
     grid["density"] = grid.geometry.apply(clip_density)
     grid["density"] = grid["density"].replace(0, np.nan)  # Set 0's to nan so they don't draw on map
