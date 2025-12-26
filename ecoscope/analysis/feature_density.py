b08f13ec93ac6ecc43dcf6d4a7acffa25aaefb4b
Alex Morling <atmorling@gmail.com>
2025-12-26 12:23:41 +0200

    add docs and cleanup

--- ecoscope/analysis/feature_density.py
+++ ecoscope/analysis/feature_density.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Callable, Literal
 
 import geopandas as gpd  # type: ignore[import-untyped]
 import numpy as np
@@ -9,9 +9,25 @@ def calculate_feature_density(
     selection: gpd.GeoDataFrame,
     grid: gpd.GeoDataFrame,
     geometry_type: Literal["point", "line"] = "point",
-    aggregate: str | None = None,
+    aggregate: str | Callable | None = None,
     aggregate_column: str | None = None,
 ) -> gpd.GeoDataFrame:
+    """
+    Computes the aggregate value for the selection gdf over the provided mesh grid.
+
+    Args:
+    selection (gpd.GeoDatFrame): The data.
+    grid (gpd.GeoDatFrame): The mesh grid to bin the selection geometry into.
+    geometry_type (Literal["point", "line"]): Whether to operate on point or line geometry.
+    aggregate: (str | Callable | None): The aggregate function to perform on each grid cell, ie "count", "sum" etc
+        Defaults to "count" if geometry_type is "point" and "sum" if geomtery type is "line"
+    aggregate_column(str | None): If provided, run the aggregate function on this column instead of the geometry.
+        If not provided and geometry_type is "point", this function will count the geometry in each grid cell
+        If not provided and geometry_type is "line", this function will run the aggregate over the total length
+            of geometry in each grid cell.
+    Returns:
+    The input dataframe with a color map appended.
+    """
     # project selection data to the crs of the provided grid
     selection = selection.to_crs(grid.crs)
 
@@ -22,8 +38,12 @@ def calculate_feature_density(
         if geometry_type == "point":
             cell_mask = selection.geometry.within(cell)
             result = selection[cell_mask]
-            aggregate_func = aggregate if aggregate else "count"
-            series_to_aggregate = result[aggregate_column] if aggregate_column else result[cell_mask]
+            if aggregate_column:
+                aggregate_func = aggregate if aggregate else "count"
+                series_to_aggregate = result[aggregate_column]
+            else:
+                aggregate_func = "count"
+                series_to_aggregate = result[cell_mask]
         elif geometry_type == "line":
             result = selection.clip_by_rect(*cell.bounds)
             result = result[~result.is_empty]
