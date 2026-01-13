fbad84d15515049052c05c5df5e78ef9cff16e85
Alex Morling <atmorling@gmail.com>
2026-01-13 15:22:40 +0200

    update ltd to use apply_spatial_classficiation

--- ecoscope/analysis/linear_time_density.py
+++ ecoscope/analysis/linear_time_density.py
@@ -23,32 +23,14 @@ def calculate_ltd(
     if grid.crs.is_projected and grid.crs.axis_info[0].unit_name != "metre":
         raise ValueError("Projected grid crs must be in metres")
 
-    # project traj to the crs of the provided grid
-    traj.gdf = traj.gdf.to_crs(grid.crs)
-    # We need the grid cell index later to count density values
-    grid = grid.reset_index().rename(columns={"index": "grid_cell_index"})
-    overlay = grid.overlay(traj.gdf, how="intersection", keep_geom_type=False)
+    classified_traj = traj.apply_spatial_classification(grid)
 
-    # drop anything that isn't a line
-    overlay = overlay[overlay.geometry.type == "LineString"]
+    ltd_grid = grid.reset_index().rename(columns={"index": "grid_cell_index"})
 
-    # use the length of the linestring if our crs is projected
-    # otherwise get great circle distance from Geod
-    if overlay.crs.is_projected:
-        overlay["fragment_distance"] = overlay["geometry"].apply(lambda x: x.length)
-    else:
-        overlay["fragment_distance"] = overlay["geometry"].apply(
-            lambda x: Geod(ellps="WGS84").inv(*x.coords[0], *x.coords[1])[2]
-        )  # Geod.inv returns tuple where the 3rd value is the distance in metres
+    density = classified_traj.gdf.groupby("spatial_index")["timespan_seconds"].sum()
+    ltd_grid["density"] = ltd_grid["grid_cell_index"].map(density)
 
-    # fragment_distance is in metres so convert speed to 'meters per hour'
-    # then multiply by 3600 to get time in seconds
-    overlay["fragment_time"] = (overlay["fragment_distance"] / (overlay["speed_kmhr"] * 1000)) * 3600
+    total_time = round(ltd_grid["density"].sum(), 1)
 
-    # This is our time density value
-    grid["density"] = overlay.groupby("grid_cell_index")["fragment_time"].sum()
-
-    total_time = round(grid["density"].sum(), 1)
-
-    grid["density"] = grid["density"] / total_time
-    return grid
+    ltd_grid["density"] = ltd_grid["density"] / total_time
+    return ltd_grid
