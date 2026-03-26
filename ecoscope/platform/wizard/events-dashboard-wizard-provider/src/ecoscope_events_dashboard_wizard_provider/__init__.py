0638865f4bd1ed9993789d208c406c9903ed9f95
Alex Morling <atmorling@gmail.com>
2026-03-26 15:41:58 +0200

    src was gitignored

---
+++ ecoscope/platform/wizard/events-dashboard-wizard-provider/src/ecoscope_events_dashboard_wizard_provider/__init__.py
@@ -0,0 +1,7 @@
+"""Ecoscope events dashboard wizard provider."""
+
+from ecoscope_events_dashboard_wizard_provider.provider import (
+    EcoscopeEventsDashboardWizardProvider,
+)
+
+__all__ = ["EcoscopeEventsDashboardWizardProvider"]
