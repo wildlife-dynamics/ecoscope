d4efcfab677245403dd85cd5d6f7569dcd09186b
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2026-03-30 10:18:06 -0400

    reset wizard dir for merge against new branch

--- wizard/ecocope-wizard-providers/events-map-example/src/ecoscope_events_map_example/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Ecoscope events map example wizard provider."""
-
-from ecoscope_events_map_example.provider import (
-    EcoscopeEventsMapExampleProvider,
-)
-
-__all__ = ["EcoscopeEventsMapExampleProvider"]
