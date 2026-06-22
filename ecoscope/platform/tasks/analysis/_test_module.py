9b30c63d8c26211e0248d5b583473441a4a03584
Alex Morling <atmorling@gmail.com>
2026-06-22 18:52:34 +0200

    annotation

--- ecoscope/platform/tasks/analysis/_test_module.py
+++ ecoscope/platform/tasks/analysis/_test_module.py
@@ -2,7 +2,7 @@ from wt_registry import register
 
 
 @register()
-def test_function():
+def test_function() -> str:
     from ecoscope.analysis.test_deep.folder.breaks_windows.module import test_func
 
     return test_func()
