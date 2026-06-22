310ff716b714c470e08c163c209f80bee6264dd3
Alex Morling <atmorling@gmail.com>
2026-06-22 19:06:10 +0200

    test if adding input helps

--- ecoscope/platform/tasks/analysis/_test_module.py
+++ ecoscope/platform/tasks/analysis/_test_module.py
@@ -2,7 +2,7 @@ from wt_registry import register
 
 
 @register()
-def test_function() -> str:
+def test_function(name: str) -> str:
     from ecoscope.analysis.test_deep.folder.breaks_windows.module import test_func
 
-    return test_func()
+    return f"{name}_{test_func()}"
