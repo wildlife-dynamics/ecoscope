2410061f6fbe8b3e0eb1d319d5cd6c7e6d77f943
Alex Morling <atmorling@gmail.com>
2026-06-23 13:46:02 +0200

    pretend version

--- publish/recipes/vendor/astropy-base/bld.bat
+++ publish/recipes/vendor/astropy-base/bld.bat
@@ -1,5 +1,8 @@
 @echo on
 
+set "SETUPTOOLS_SCM_PRETEND_VERSION_FOR_ASTROPY=%PKG_VERSION%"
+set "SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%"
+
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
 if errorlevel 1 exit 1
 
